c4576238b14319cafe2fcd7b1dc26e6814cfb143
martina97 <Martinademaio97>
2021-03-20 16:39:13 +0100

    prese tutte resolution date dei ticket

--- src/deliverable/Main.java
+++ src/deliverable/Main.java
@@ -2,13 +2,18 @@ package deliverable;
 
 import java.util.logging.Logger;
 
+import org.json.JSONException;
+
+import java.io.IOException;
 import java.util.logging.Level;
 
 public class Main {
-	static Logger logger = Logger.getLogger(Main.class.getName());
+	//static Logger logger = Logger.getLogger(Main.class.getName());
 	
-   public static void main(String[] args) {
-	    logger.log(Level.INFO, "matteo"); // oppure  logger.info("matteo");
+   public static void main(String[] args) throws IOException, JSONException {
+	    //logger.log(Level.INFO, "matteo"); // oppure  logger.info("matteo");
 	    //se errore -->  logger.severe("matteo");
+	   getReleaseInfo.main(args);
+	   RetrieveTicketsID.main(args);
    }
 }
---
+++ src/deliverable/RetrieveTicketsID.java
@@ -0,0 +1,85 @@
+package deliverable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+public class RetrieveTicketsID {
+
+
+
+
+   private static String readAll(Reader rd) throws IOException {
+	      StringBuilder sb = new StringBuilder();
+	      int cp;
+	      while ((cp = rd.read()) != -1) {
+	         sb.append((char) cp);
+	      }
+	      return sb.toString();
+	   }
+
+   public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
+      InputStream is = new URL(url).openStream();
+      try {
+         BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+         String jsonText = readAll(rd);
+         JSONArray json = new JSONArray(jsonText);
+         return json;
+       } finally {
+         is.close();
+       }
+   }
+
+   public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+      InputStream is = new URL(url).openStream();
+      try {
+         BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+         String jsonText = readAll(rd);
+         JSONObject json = new JSONObject(jsonText);
+         return json;
+       } finally {
+         is.close();
+       }
+   }
+
+
+  public static void main(String[] args) throws IOException, JSONException {
+	  
+	  String projName ="DAFFODIL";
+	   Integer j = 0, i = 0, total = 1;
+	   JSONArray issues ;
+      //Get JSON API for closed bugs w/ AV in the project
+      do {
+         //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
+         j = i + 1000;
+         String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
+                + projName + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
+                + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
+                + i.toString() + "&maxResults=" + j.toString();
+         JSONObject json = readJsonFromUrl(url);
+         
+         issues = json.getJSONArray("issues");
+         total = json.getInt("total");
+         for (; i < total && i < j; i++) {
+            //Iterate through each bug
+            String key = issues.getJSONObject(i%1000).get("key").toString();
+            String data = issues.getJSONObject(i).getJSONObject("fields").getString("resolutiondate");
+            
+            System.out.println(key + "\tdata = " + data);
+         }  
+      }
+      
+      while (i < total);      
+   }
+
+ 
+}
